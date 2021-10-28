5f300b81b749a3e1fa338858663b649b0dde8996
Polina Moiseeva <moiseeva.polina@gmail.com>
2021-10-28 21:27:17 +0400

    Add connect to Wi-Fi

--- sketch_oct26a.ino
+++ sketch_oct26a.ino
@@ -1,22 +1,42 @@
 #include "TM1637.h"
+#include <ESP8266WiFi.h>
 
-TM1637 tm1637(12, 14);              // CLK, DIO (D6, D5)
+TM1637 tm1637(12, 14); // CLK, DIO (D6, D5)
 
-void setup() {
-  tm1637.init();                      ///tm1637
+char ssid[] = "SnowFish";             //  your network SSID (name)
+char pass[] = "38051686";        // your network password
+
+void setup()
+{
+  tm1637.init(); ///tm1637
   tm1637.set(7);
   //tm1637.clearDisplay();
   //tm1637.display(2,2);
-  Serial.begin(115200); 
+  Serial.begin(115200);
   Serial.println("Start");
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
+void loop()
+{
 
+  //int8_t TimeDisp[4];
 
- //int8_t TimeDisp[4]; 
-
-  for (float i = 0; i <= 9999; i++) {
+  for (float i = 0; i <= 9999; i++)
+  {
     // tm1637.display(i);
     tm1637.clearDisplay();
 
@@ -31,18 +51,18 @@ void loop() {
 
     //tm1637.display(TimeDisp);
     if (razr0 != 0)
-      tm1637.display(0,razr0);
-      /*Serial.print("0-");
+      tm1637.display(0, razr0);
+    /*Serial.print("0-");
       Serial.println(razr0);*/
 
     if (!(razr0 == 0 && razr1 == 0))
-      tm1637.display(1,razr1);
+      tm1637.display(1, razr1);
 
     if (!(razr0 == 0 && razr1 == 0 && razr2 == 0))
-      tm1637.display(2,razr2);
+      tm1637.display(2, razr2);
 
-    tm1637.display(3,razr3);
+    tm1637.display(3, razr3);
 
     delay(500);
-  } 
+  }
 }
